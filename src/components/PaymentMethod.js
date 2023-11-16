b5d74d467a782b1ed3e16fad88ceb7a60ccf592f
abiolatom <107856136+abiolatom@users.noreply.github.com>
2023-11-16 15:34:38 +0200

    Almost getting Payment method

--- src/components/PaymentMethod.js
+++ src/components/PaymentMethod.js
@@ -4,35 +4,51 @@ import { ProductContext } from "./ProductContext";
 const PaymentMethod = () => {
   const [selectedOptions, setSelectedOptions] = useState([]);
   const [inputValues, setInputValues] = useState([]);
-  const {calculateTotalPrice} = useContext(ProductContext);
+  const { calculateTotalPrice } = useContext(ProductContext);
 
-  const handleOptionChange = (e) => {
-    const selectedOption = e.target.value;
-    const newSelectedOption = [...selectedOptions, { selectedOption }];
-    setSelectedOptions(newSelectedOption);
+  const handleOptionChange = (option) => (e) => {
+    const isChecked = e.target.checked;
 
-    setInputValues({
-      ...inputValues,
-      [selectedOption]: "",
+    setSelectedOptions((prevSelectedOptions) => {
+      if (isChecked) {
+        return [...prevSelectedOptions, option];
+      } else {
+        const updatedOptions = prevSelectedOptions.filter(
+          (selectedOption) => selectedOption !== option
+        );
+        const { [option]: removedValue, ...remainingValues } = inputValues;
+        setInputValues(remainingValues);
+
+        return updatedOptions;
+      }
     });
   };
 
-  const handleInputChange = (e, selectedOption) => {
-    const value = e.target.value;
+  const handleInputChange = (option) => (e) => {
+    const newValue = e.target.value;
 
-    setInputValues({
-      ...inputValues,
-      [selectedOption]: value,
-    });
+    setInputValues((prevInputValues) => ({
+      ...prevInputValues,
+      [option]: newValue,
+    }));
   };
 
-  
   return (
     <div>
       <h1>Payment Information</h1>
-      <p><em>Amount to pay:</em> <b>{calculateTotalPrice().toFixed(2)}</b></p>
+      <p>
+        <em>Amount to pay:</em> <b>{calculateTotalPrice().toFixed(2)}</b>
+      </p>
       <label>Select Payment Method</label>{" "}
-      <select multiple onChange={handleOptionChange}>
+      <select
+        multiple={true}
+        value={selectedOptions}
+        onChange={(e) =>
+          setSelectedOptions(
+            Array.from(e.target.selectedOptions, (option) => option.value)
+          )
+        }
+      >
         <option>Cash...</option>
         <option>Bank Transfer...</option>
         <option>POS...</option>
@@ -40,12 +56,14 @@ const PaymentMethod = () => {
       <div>
         {selectedOptions.map((option) => (
           <div key={option}>
-            <label>{option}:</label>
-            <input
-              type="text"
-              value={inputValues[option] || ""}
-              onChange={(e) => handleInputChange(e, option)}
-            />
+            <label>
+              {option}:
+              <input
+                type="text"
+                value={inputValues[option] || ""}
+                onChange={handleInputChange(option)}
+              />
+            </label>
           </div>
         ))}
       </div>
