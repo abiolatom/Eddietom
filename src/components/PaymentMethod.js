d9be7a266054744cb32178707aac1a8966d3e33d
abiolatom <107856136+abiolatom@users.noreply.github.com>
2023-11-21 12:32:53 +0200

    making changes to handleOption Change

--- src/components/PaymentMethod.js
+++ src/components/PaymentMethod.js
@@ -4,12 +4,19 @@ import { ProductContext } from "./ProductContext";
 const PaymentMethod = () => {
   const [selectedOptions, setSelectedOptions] = useState([]);
   const { calculateTotalPrice } = useContext(ProductContext);
+  const [amounts, setAmounts] = useState({});
 
   const options = [
     { value: "cash", label: "Cash" },
     { value: "transfer", label: "Bank Transfer" },
     { value: "pos", label: "POS" },
   ];
+
+  const handleAmountChange = (event, value) => {
+    const newAmount = { ...amounts };
+    newAmount[value] = event.target.value;
+    setAmounts(newAmount);
+  };
 
   const handleOptionChange = (value) => {
     const newSelectedOptions = selectedOptions.includes(value)
@@ -19,8 +26,20 @@ const PaymentMethod = () => {
   };
 
   const optionsRender = () => {
+    const totalPayment = Object.values(amounts).reduce(
+      (acc, amount) => acc + parseFloat(amount) || 0,
+      0
+    );
     return options.map((option) => {
       const isChecked = selectedOptions.includes(option.value);
+      const paymentInput = isChecked ? (
+        <input
+          type="number"
+          placeholder={`Enter amount paid by ${option.label}`}
+          value={amounts[option.value] || ""}
+          onChange={(e) => handleAmountChange(e, option.value)}
+        />
+      ) : null;
       return (
         <div key={option.value}>
           <input
@@ -30,16 +49,25 @@ const PaymentMethod = () => {
             value={option.value}
           />
           <label>{option.label} </label>
-          {isChecked && (
-            <input
-              type="number"
-              placeholder={`Enter amount paid by ${option.label}`}
-            />
-          )}
+          {paymentInput}
         </div>
       );
     });
   };
+
+  const paymentComparison = () => {
+    if (totalPayment === calculateTotalPrice()) {
+      return "Total payment matches required amount.";
+    } else if (totalPayment > calculateTotalPrice()) {
+      return "Total payment is more than required amount.";
+    } else {
+      return "Total payment is less than required amount.";
+    }
+  };
+  const totalPayment = Object.values(amounts).reduce(
+    (acc, amount) => acc + parseFloat(amount) || 0,
+    0
+  );
 
   return (
     <div>
@@ -47,8 +75,12 @@ const PaymentMethod = () => {
       <p>
         <em>Amount to pay:</em> <b>{calculateTotalPrice().toFixed(2)}</b>
       </p>
+      <p>
+        <em>Total paid:</em> <b>{totalPayment.toFixed(2)}</b>
+      </p>
+      <p>{paymentComparison()}</p>
       <label>Select Payment Method</label> {optionsRender()}
-      <p>Payment Options: {selectedOptions.join(", ")} </p>
+      <p>Payment Options: {selectedOptions.join(", ")}</p>
     </div>
   );
 };
