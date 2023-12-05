5cfe83e24d0a8013c92fd1096ba3053fc4d1b804
abiolatom <107856136+abiolatom@users.noreply.github.com>
2023-12-05 19:24:15 +0200

    remaining to render on page

--- src/utils/codesForLaterUse.js
+++ src/utils/codesForLaterUse.js
@@ -56,3 +56,46 @@ const PaymentMethod = () => {
 };
 
 export default PaymentMethod;
+
+
+mongoose
+.connect(MONGO_URI)
+.then(() => console.log("Connected to MongoDB"))
+.catch((error) => console.error("Error connecting to MongoDB", error));
+
+Router.get("/", async (req, res) => {
+const products = await Products.find();
+res.json(products);
+});
+}
+
+Router.post("/", async (req, res) => {
+try {
+const newProducts = new Products({
+  Product_Name: req.body.productName,
+  Unit_Cost: req.body.unitCost,
+  Quantity: req.body.quantity,
+  Transport_Cost: req.body.transportCost,
+  Total_Cost: req.body.totalCost,
+  Purchase_Date: req.body.purchaseDate,
+  Delivery_Date: req.body.deliveryDate,
+  Seller_Name: req.body.sellerName,
+  Seller_Address: req.body.sellerAddress,
+  Payment_Method: req.body.paymentMethod,
+  Payment_Date: req.body.paymentDate,
+  Payment_Installment: req.body.paymentInstallment,
+  Second_Installment_Date: req.body.secondInstallment,
+  Payment_Completion_Date: req.body.paymentCompletionDate,
+  Other_Info: req.body.otherInfo,
+});
+await newProducts.save();
+res.json(newProducts);
+} catch (err) {
+console.error("Error saving product:", err);
+res.status(500).json({ error: "Server error" });
+}
+});
+
+app.listen(3001, () => {
+console.log(`Server listening on port ${PORT}`);
+});
