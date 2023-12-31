3a7b8ed06dfbda5e7d5ed6d9ac5aab38b336f047
abiolatom <107856136+abiolatom@users.noreply.github.com>
2023-12-31 09:49:22 +0100

    working on DebtSales form

--- src/components/SalesDeposit.js
+++ src/components/SalesDeposit.js
@@ -20,7 +20,7 @@ const SalesDeposit = () => {
         className="bg-blue-500 mt-2 text-white px-4 py-2 rounded-md hover:bg-blue-600"
         onClick={() => navigate("/")}
       >
-        {" "}
+       
         Modify Products
       </button>
       {selectedProducts.length > 0 && (
