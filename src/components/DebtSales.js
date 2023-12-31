7b8248ed5bd5cd71e1237d8c0a75c4175bafee51
abiolatom <107856136+abiolatom@users.noreply.github.com>
2023-12-31 10:12:29 +0100

    on it

--- src/components/DebtSales.js
+++ src/components/DebtSales.js
@@ -13,6 +13,50 @@ const DebtSales = () => {
     setAmounts,
   } = useContext(ProductContext);
 
+  const [cashPayment, setCashPayment] = useState(0);
+  const [bankPayment, setBankPayment] = useState(0);
+  const [bankName, setBankName] = useState('');
+  const [posPayment, setPosPayment] = useState(0);
+  const [balance, setBalance] = useState(0);
+  const [installments, setInstallments] = useState(1);
+  const [dates, setDates] = useState([]);
+  const totalAmount = calculateTotalPrice();
+
+  function handleBankPaymentChange(event) {
+    setBankPayment(event.target.value);
+  }
+
+  function handleBankNameChange(event) {
+    setBankName(event.target.value);
+  }
+
+  function handlePosPaymentChange(event) {
+    setPosPayment(event.target.value);
+  }
+
+  function handleInstallmentsChange(event) {
+    setInstallments(event.target.value);
+  }
+
+  function handleDateChange(index, event) {
+    const newDates = [...dates];
+    newDates[index] = event.target.value;
+    setDates(newDates);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form data:", {
+      cashPayment,
+      bankPayment,
+      bankName,
+      posPayment,
+     
+      installments,
+     
+    });
+  };
+
   return (
     <div>
       <h2>Debt Sales Form</h2>
@@ -45,9 +89,47 @@ const DebtSales = () => {
           </div>
         </div>
       )}
-      <form>
-        
-      </form>
+       <form onSubmit={handleSubmit}>
+      <div>
+        <label>Cash Payment:</label>
+        <input type="number" value={cashPayment} onChange={(e) => setCashPayment(e.target.value)} />
+      </div>
+      <div>
+        <label>Bank Payment:</label>
+        <input type="number" value={bankPayment} onChange={handleBankPaymentChange} />
+        {bankPayment > 0 && (
+          <input type="text" placeholder="Bank Name" value={bankName} onChange={handleBankNameChange} />
+        )}
+      </div>
+      <div>
+        <label>POS Payment:</label>
+        <input type="number" value={posPayment} onChange={handlePosPaymentChange} />
+      </div>
+      <div>
+        <label>Total Amount:</label>
+        <span>{totalAmount}</span>
+      </div>
+      <div>
+        <label>Balance:</label>
+        <input type="number" value={balance} readOnly />
+      </div>
+      <div>
+        <label>Installments:</label>
+        <select value={installments} onChange={handleInstallmentsChange}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+        </select>
+      </div>
+      {Array.from({ length: installments }).map((_, index) => (
+        <div key={index}>
+          <label>Date for Installment {index + 1}:</label>
+          <input type="date" value={dates[index] || ''} onChange={(e) => handleDateChange(index, e)} />
+          {/* You may want to add an input for the amount of each installment here */}
+        </div>
+      ))}
+      <button type="submit">Submit</button>
+    </form>
     </div>
   );
 };
